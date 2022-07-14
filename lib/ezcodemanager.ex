b77683fa3fc764d9be8e309ceb77753ed98b11cc
Matt Evans <81327996+nhpip@users.noreply.github.com>
2022-07-14 08:34:13 -0400

    wip

--- lib/ezcodemanager.ex
+++ lib/ezcodemanager.ex
@@ -175,6 +175,7 @@ defmodule EZProfiler.Manager do
 
   defp do_stop_ezprofiler(pid, count) do
     Process.sleep(100)
+    IO.inspect(:xxxxx)
     ! Process.info(pid) || do_stop_ezprofiler(pid, count - 1)
   end
 
